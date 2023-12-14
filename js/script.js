b46a35e5661e451c7903c65f380a2170ba105fbe
Prathap-Siva <prathapsendto@gmail.com>
2023-12-14 16:41:19 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let inputUrl = document.getElementById('inputUrl')
+let inputUrlBtn = document.getElementById('inputUrlBtn')
+
+
+inputUrlBtn.addEventListener('click',()=>{
+    console.log(inputUrl.value);
+})
